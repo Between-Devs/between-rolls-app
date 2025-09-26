c29293d2c7b625966c928c86e07d7b2fda9d00f0
Landro <ljborges76@gmail.com>
2025-09-26 16:52:19 -0300

    feat: add `PublicNavbar` in public layout

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,10 +1,13 @@
+import { PublicNavbar } from '@/components/common/PublicNavbar';
+
 export default function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="font-mukta flex min-h-screen flex-col">
+      <PublicNavbar />
       <main className="flex-grow">{children}</main>
     </div>
   );
