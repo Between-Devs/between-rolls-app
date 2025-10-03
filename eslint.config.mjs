b58e2ca20643cbd622f25a32b15c0378fd3d18c8
Landro <ljborges76@gmail.com>
2025-10-03 16:39:10 -0300

    feat: add and setup `storybook`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
@@ -12,23 +15,18 @@ const compat = new FlatCompat({
   recommendedConfig: js.configs.recommended,
 });
 
-const eslintConfig = [
-  ...compat.extends(
-    'eslint:recommended',
-    'next/core-web-vitals',
-    'next/typescript',
-  ),
-  {
-    ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
-    ],
-  },
-
-  prettierConfig,
-];
+const eslintConfig = [...compat.extends(
+  'eslint:recommended',
+  'next/core-web-vitals',
+  'next/typescript',
+), {
+  ignores: [
+    'node_modules/**',
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ],
+}, prettierConfig, ...storybook.configs["flat/recommended"]];
 
 export default eslintConfig;
